5a263c44b6a49f7ba4f44362839631ff6da153d0
Dysky2 <dawid4799@gmail.com>
2026-07-23 15:35:50 +0200

    Add response for data violation

--- src/main/java/net/dysky/planner/exception/GlobalExceptionHandler.java
+++ src/main/java/net/dysky/planner/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package net.dysky.planner.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import net.dysky.planner.response.ResponseDTO;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -37,6 +38,27 @@ public class GlobalExceptionHandler {
         );
 
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ResponseDTO> handleDataIntegrityViolation(DataIntegrityViolationException ex, HttpServletRequest request) {
+        String message = "Data integrity violation occurred.";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        if (ex.getMessage() != null && ex.getMessage().contains("uk_trip_name_and_creator")) {
+            message = "You already have a trip with this name. Please choose a different name.";
+            status = HttpStatus.CONFLICT;
+        }
+
+        ResponseDTO error = new ResponseDTO(
+                LocalDateTime.now(),
+                status.value(),
+                message,
+                request.getRequestURI(),
+                null
+        );
+
+        return ResponseEntity.status(status).body(error);
     }
 
     @ExceptionHandler(Exception.class)
