66c60a479d6d651e36bd5b85686826df66551b31
Dysky2 <dawid4799@gmail.com>
2026-07-23 12:34:16 +0200

    Add config for swagger

--- src/main/java/net/dysky/planner/PlannerApplication.java
+++ src/main/java/net/dysky/planner/PlannerApplication.java
@@ -1,11 +1,26 @@
 package net.dysky.planner;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableRetry
+@OpenAPIDefinition(
+        info = @Info(title = "Planner API", version = "1.0"),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class PlannerApplication {
 
     public static void main(String[] args) {
