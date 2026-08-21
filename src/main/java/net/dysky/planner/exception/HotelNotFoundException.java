da550fd63d3e60f2fc8b1c22d50afe9cbb7a0866
Dysky2 <dawid4799@gmail.com>
2026-08-21 19:41:01 +0200

    Delete unused exception

--- src/main/java/net/dysky/planner/exception/HotelNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package net.dysky.planner.exception;
-
-public class HotelNotFoundException extends RuntimeException {
-    public HotelNotFoundException(String message) {
-        super(message);
-    }
-}
