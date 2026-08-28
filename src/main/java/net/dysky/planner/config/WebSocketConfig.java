b3b7e47ca6541f09b7d4d6897b4dfad1baa34ba3
Dysky2 <dawid4799@gmail.com>
2026-08-28 19:27:27 +0200

    PLR-43: fix

--- src/main/java/net/dysky/planner/config/WebSocketConfig.java
+++ src/main/java/net/dysky/planner/config/WebSocketConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Value("${FRONTEND_URL}")
+    @Value("${app.frontend.url}")
     private String allowedOrigins;
 
     @Override
