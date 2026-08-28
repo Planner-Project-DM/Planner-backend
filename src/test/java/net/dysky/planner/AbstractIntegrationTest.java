30d8ba1637217fc37ce67578d8ed3e8aff9bfd7b
Dysky2 <dawid4799@gmail.com>
2026-08-28 19:17:12 +0200

    PLR-43: fix tests

--- src/test/java/net/dysky/planner/AbstractIntegrationTest.java
+++ src/test/java/net/dysky/planner/AbstractIntegrationTest.java
@@ -3,12 +3,14 @@ package net.dysky.planner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1)
 public abstract class AbstractIntegrationTest {
 
     @Autowired
--- src/test/java/net/dysky/planner/PlannerApplicationTests.java
+++ src/test/java/net/dysky/planner/PlannerApplicationTests.java
@@ -7,10 +7,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@EmbeddedKafka(
-        partitions = 1,
-        brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" }
-)
+@EmbeddedKafka(partitions = 1)
 class PlannerApplicationTests {
 
     @Test
