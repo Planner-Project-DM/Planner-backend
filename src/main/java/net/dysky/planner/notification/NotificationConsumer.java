d7ae81e6dbdc14153008fcaee88ce002d117e36b
Dysky2 <dawid4799@gmail.com>
2026-08-28 16:28:46 +0200

    PLR-43: Add sending message into websocket

--- src/main/java/net/dysky/planner/notification/NotificationConsumer.java
+++ src/main/java/net/dysky/planner/notification/NotificationConsumer.java
@@ -1,13 +1,20 @@
 package net.dysky.planner.notification;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class NotificationConsumer {
+
+    private final SimpMessagingTemplate messagingTemplate;
 
     @KafkaListener(topics = "planner-notification", groupId = "notification-group")
     public void consume(NotificationDTO dto) {
-        System.out.println(dto.message());
+        String userId = dto.receiverId().toString();
+
+        messagingTemplate.convertAndSendToUser(userId, "/queue/notifications", dto);
     }
 }
