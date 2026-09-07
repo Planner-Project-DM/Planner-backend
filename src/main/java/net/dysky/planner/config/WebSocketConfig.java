3e251338a7d904bf3f7cc04ad21d0e4a3eddace5
Dysky2 <dawid4799@gmail.com>
2026-09-07 19:23:14 +0200

    PLR-70: Add better config for websocket

--- src/main/java/net/dysky/planner/config/WebSocketConfig.java
+++ src/main/java/net/dysky/planner/config/WebSocketConfig.java
@@ -1,18 +1,41 @@
 package net.dysky.planner.config;
 
+import lombok.RequiredArgsConstructor;
+import net.dysky.planner.auth.JwtService;
+import net.dysky.planner.user.User;
+import net.dysky.planner.user.UserService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.security.Principal;
+import java.util.List;
+
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Value("${app.frontend.url}")
     private String allowedOrigins;
+
+    private final JwtService jwtService;
+    private final UserService userService;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -26,6 +49,37 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/topic", "/queue");
         registry.setApplicationDestinationPrefixes("/app");
         registry.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+                if(StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    String authHeader = accessor.getFirstNativeHeader("Authorization");
+
+                    if(authHeader != null && authHeader.startsWith("Bearer ")) {
+                        String jwt = authHeader.substring(7);
+                        String userEmail = jwtService.extractEmail(jwt);
+
+                        if(userEmail != null) {
+                            User user = userService.getUserByEmail(userEmail);
+
+                            if (jwtService.isTokenValid(jwt, user)) {
+                                Principal principal = () -> user.getId().toString();
+
+                                accessor.setUser(principal );
+                            }
+                        }
+                    }
+                }
+                return message;
+            }
+        });
     }
 
 }
