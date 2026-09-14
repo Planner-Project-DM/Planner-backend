7cc5b74f13cc26b1b35ea9b28848cdd02b151254
Dysky2 <dawid4799@gmail.com>
2026-09-14 18:36:45 +0200

    PLR-84: Fix N+1 problem in trips

--- src/main/java/net/dysky/planner/trip/TripRepository.java
+++ src/main/java/net/dysky/planner/trip/TripRepository.java
@@ -1,17 +1,28 @@
 package net.dysky.planner.trip;
 
 import net.dysky.planner.group.Group;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 interface TripRepository extends JpaRepository<Trip, UUID> {
+
+    @EntityGraph(attributePaths = {"tripGroup", "tripCreator", "tripItineraries"})
+    Optional<Trip> findById(UUID id);
+
     List<Trip> findByStatus(TripStatus status);
 
+    @EntityGraph(attributePaths = {"tripGroup", "tripCreator"})
     List<Trip> findAllByTripCreatorEmail(String tripCreatorEmail);
 
+    @EntityGraph(attributePaths = {"tripGroup", "tripCreator"})
     List<Trip> findAllByTripCreatorEmailAndStatus(String tripCreatorEmail, TripStatus status);
+
+    List<Trip> findAllByTripGroupIn(Collection<Group> groups);
 
     boolean existsByNameAndTripCreator_Email(String name, String tripCreatorEmail);
 
