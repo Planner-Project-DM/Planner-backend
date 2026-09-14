e55510abb4d1d8d73b03baeb003cb4817b713963
Dysky2 <dawid4799@gmail.com>
2026-09-14 18:39:24 +0200

    PLR-84: Fix N+1 problem in friendships

--- src/main/java/net/dysky/planner/friendship/FriendshipRepository.java
+++ src/main/java/net/dysky/planner/friendship/FriendshipRepository.java
@@ -1,6 +1,7 @@
 package net.dysky.planner.friendship;
 
 import net.dysky.planner.user.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,9 +11,12 @@ import java.util.Optional;
 import java.util.UUID;
 
 interface FriendshipRepository extends JpaRepository<Friendship, UUID> {
+
+    @EntityGraph(attributePaths = {"userSender", "userReceiver"})
     @Query("SELECT f from Friendship f WHERE (f.userSender = :user OR f.userReceiver = :user) AND f.status = 'ACCEPTED'")
     List<Friendship> findAllByUserFriends(@Param("user") User user);
 
+    @EntityGraph(attributePaths = {"userSender", "userReceiver"})
     @Query("SELECT f from Friendship f WHERE (f.userSender = :user OR f.userReceiver = :user) AND f.status = :status")
     List<Friendship> findAllByUserFriendsAndStatus(@Param("user") User user, @Param("status") FriendshipStatus status);
 
@@ -27,5 +31,6 @@ interface FriendshipRepository extends JpaRepository<Friendship, UUID> {
             "(f.userSender = :friend AND f.userReceiver = :user))")
     boolean existsFriendshipBy(@Param("user") User user, @Param("friend") User friend);
 
+    @EntityGraph(attributePaths = {"userSender", "userReceiver"})
     List<Friendship> findByUserReceiver_EmailAndStatus(String userReceiverEmail, FriendshipStatus status);
 }
