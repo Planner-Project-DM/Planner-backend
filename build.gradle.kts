386f303a4b17ec8b853ed247b12dfda25b19398f
Dysky2 <dawid4799@gmail.com>
2026-09-15 19:05:58 +0200

    PLR-85: Add cache config

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
 
     implementation("com.bucket4j:bucket4j-core:8.10.1")
     implementation("com.github.ben-manes.caffeine:caffeine")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
